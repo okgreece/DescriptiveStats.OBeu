37f9cbe7d7b7b4b5b4865bfa0840e0e6a067aa9c
kleanthisk10 <akis_k10@yahoo.com>
2016-11-10 03:53:35 +0200

    update

--- R/open_spending.ds.R
+++ R/open_spending.ds.R
@@ -4,10 +4,13 @@
 #' @description
 #' Extract and analyze the input data provided from Open Spending API, using the ds.analysis function.
 #' 
-#' @usage open_spending.ds(json_data,amount)
+#' @usage open_spending.ds(json_data, box.outl=1.5, cor.method= "pearson", select=NULL)
 #' 
 #' @param json_data The json string, URL or file from Open Spending API
-#' @param amount Specify the amount label of the json input data
+#' @param box.outl ...
+#' @param cor.method The correlation coefficient method to compute: "pearson" (default),
+#' "kendall" or "spearman".
+#' @param select One or more nominal variables to calculate their corresponding frequencies.
 #' 
 #' @details 
 #' This function is used to read data in json format from Open Spending API, in order to implement 
@@ -22,19 +25,32 @@
 #' @rdname open_spending.ds
 #' 
 #' @import jsonlite
+#' @import reshape
 #' 
 #' @export
 #####################################################################################################
 
-open_spending.ds <- function(json_data,amount){
-  
+open_spending.ds <- function(json_data, box.outl=1.5, cor.method= "pearson", select=NULL){
+
   dt <- jsonlite::fromJSON(json_data)
+
+  dimensions=c("data","cells")
   
-  dt<-dt["data"]
+  select.dim=match.arg(dimensions,names(dt),several.ok = T)
   
+  dt<-dt[select.dim]
+
   dt2<-unique(as.data.frame(dt))
   
-    ds.result <- ds.analysis(dt2)
+  if (select.dim=="cells"){
+    melt <- reshape::melt.data.frame(dt2)
+    dt2 <- reshape::cast(melt, 
+                  "global__fiscalPeriod__28951.notation ~ global__budgetPhase__afd93.prefLabel",
+                  sum)
+    dt2 <- stats::na.omit(dt2)
+    
+  }
+    ds.result <- ds.analysis(dt2, box.out=box.outl, corr.method= cor.method, fr.select=select)
   
   return(ds.result)  
 }
