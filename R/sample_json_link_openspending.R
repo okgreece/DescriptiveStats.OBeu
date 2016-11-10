5ad6545984821bad99760d6052eda39f31bfb957
kleanthisk10 <akis_k10@yahoo.com>
2016-11-10 03:53:43 +0200

    update

---
+++ R/sample_json_link_openspending.R
@@ -0,0 +1,18 @@
+#' Sample data from Open Spending
+#' 
+#' @description 
+#' Sample data of Revised Budget phase amounts
+#' \itemize{
+#'   \item The year (2016) of the recorded approved budget phase amounts
+#'   \item The revised budget phase amounts of 2016
+#'   \item The original amounts of this year
+#'   \item The functional classification description
+#'   \item The functional classification code
+#'   }
+#' 
+#' @format A link with the json format data
+#' @name sample_json_link_openspending
+#' @source OpenSpending
+#' 
+#' 
+NULL
