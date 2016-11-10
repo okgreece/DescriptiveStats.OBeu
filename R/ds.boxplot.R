f70027b7b556342dc7ab8d02775c473e89855445
katerinachat <katerinachat@yahoo.com>
2016-11-10 12:18:19 +0200

    .

--- R/ds.boxplot.R
+++ R/ds.boxplot.R
@@ -2,12 +2,12 @@
 #' Boxplot
 #'  
 #' @description
-#' Boxplot
+#' This functions calculates the statistics for a boxplot of the input dataset.
 #' 
 #' @usage ds.boxplot(data, out.level=1.5)
 #' 
 #' @param data The input matrix or data frame 
-#' @param out.level ...
+#' @param out.level Determines how far the plot "whiskers" extend out from the box.
 #' 
 #' @details 
 #' This function returns the necessary statistics for bulding a boxplot
@@ -17,9 +17,10 @@
 #' @return 
 #' Returns a list with the following components:
 #' 
-#' stats: a vector with the extreme of the lower whisker, the lower ‘hinge’, the median, the upper ‘hinge’ and the extreme of the upper whisker.
+#' stats: a vector with the extreme of the lower whisker, the lower "hinge", the median, the upper "hinge" and the extreme of the upper whisker.
+#' 
 #' n: the number of non-NA observations in the sample.
-#' conf: the lower and upper extremes of the ‘notch’ 
+#' 
 #' out: the values of any data points which lie beyond the extremes of the whiskers
 #' 
 #' @seealso \code{\link{ds.analysis}}, \code{\link{open_spending.ds}}
