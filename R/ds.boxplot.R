9f9abf532b99696fce130aba25fa251bb0e3d40e
katerinachat <katerinachat@yahoo.com>
2016-11-10 11:37:04 +0200

    update

--- R/ds.boxplot.R
+++ R/ds.boxplot.R
@@ -10,12 +10,17 @@
 #' @param out.level ...
 #' 
 #' @details 
-#' boxplot
+#' This function returns the necessary statistics for bulding a boxplot
 #' 
 #' @author Aikaterini Chatzopoulou, Kleanthis Koupidis
 #' 
 #' @return 
-#' Description of the returns
+#' Returns a list with the following components:
+#' 
+#' stats: a vector with the extreme of the lower whisker, the lower ‘hinge’, the median, the upper ‘hinge’ and the extreme of the upper whisker.
+#' n: the number of non-NA observations in the sample.
+#' conf: the lower and upper extremes of the ‘notch’ 
+#' out: the values of any data points which lie beyond the extremes of the whiskers
 #' 
 #' @seealso \code{\link{ds.analysis}}, \code{\link{open_spending.ds}}
 #' 
