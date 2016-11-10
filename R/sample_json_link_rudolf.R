6b3e394314b33deaa17fe417910ca62877999bb5
kleanthisk10 <akis_k10@yahoo.com>
2016-11-10 04:15:48 +0200

    update

---
+++ R/sample_json_link_rudolf.R
@@ -0,0 +1,18 @@
+#' Sample data from Rudolf
+#' 
+#' @description 
+#' Sample data of Revised Budget phase amounts of Municipality of Athens
+#' \itemize{
+#'   \item The year (2016) of the recorded approved budget phase amounts
+#'   \item The revised budget phase amounts of 2016
+#'   \item The original amounts of this year
+#'   \item The functional classification description
+#'   \item The functional classification code
+#'   }
+#' 
+#' @format A link to json format file
+#' @name sample_json_link_rudolf
+#' @source Rudolf
+#' 
+#' 
+NULL
